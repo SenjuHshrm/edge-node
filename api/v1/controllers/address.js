c9b0419ee34fca9960f36c5117d769bf03e68f97
EnzGyb <rdimasapit.bos@gmail.com>
2022-09-02 15:58:06 +0800

    fix address lookup for jnt

--- api/v1/controllers/address.js
+++ api/v1/controllers/address.js
@@ -21,13 +21,30 @@ module.exports = {
       if(e) throw e
       // 7
       try {
-        let isAvailable = 'NO', locations = content.split('\n');
-        let paramCity = modCity(req.params.city)
-        locations.splice((locations.length - 1), 1)
-        let selectedProvince = locations.filter((x) => { return req.params.province.match(x.split(',')[1].toUpperCase()) })
-        let selectedCity = selectedProvince.filter((x) => { return x.split(',')[2].toUpperCase().match(paramCity.trim()) })
-        let selectedBrgy = selectedCity.filter((x) => { return req.params.brgy.match(x.split(',')[3].toUpperCase()) })
-        isAvailable = selectedBrgy[0].split(',')[7]
+        let isAvailable = 'NO',
+          locations = content.split('\n'),
+          paramCity = '',
+          selectedProvince = [],
+          selectedBrgy = []
+        switch(req.params.type) {
+          case 'flash':
+            paramCity = modCity(req.params.city)
+            locations.splice((locations.length - 1), 1)
+            selectedProvince = locations.filter((x) => { return req.params.province.match(x.split(',')[1].toUpperCase()) })
+            selectedCity = selectedProvince.filter((x) => { return x.split(',')[2].toUpperCase().match(paramCity.trim()) })
+            selectedBrgy = selectedCity.filter((x) => { return req.params.brgy.match(x.split(',')[3].toUpperCase()) })
+            isAvailable = selectedBrgy[0].split(',')[7]
+            break;
+          case 'jnt':
+            let province = req.params.province.replace(' ', '-')
+            paramCity = req.params.city.replace(' ', '-')
+            locations.splice((locations.length - 1), 1)
+            selectedProvince = locations.filter((x) => { return province.match(x.split(',')[4].toUpperCase()) })
+            selectedCity = selectedProvince.filter((x) => { return x.split(',')[5].toUpperCase().match(paramCity.trim()) })
+            selectedBrgy = selectedCity.filter((x) => { return req.params.brgy.match(x.split(',')[6].toUpperCase()) })
+            isAvailable = selectedBrgy[0].split(',')[7]
+            break;
+        }
         return res.status(200).json({ success: true, info: isAvailable })
       } catch(e) {
         console.log(e)
