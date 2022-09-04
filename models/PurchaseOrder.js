534ecdc7395f89b1f759bb2bf8f43c407061f011
CreativeBrainStudio <102801479+CreativeBrainStudio@users.noreply.github.com>
2022-09-04 12:57:01 +0800

    changes

--- models/PurchaseOrder.js
+++ models/PurchaseOrder.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose')
-const { ObjectId } = mongoose.Schema
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
 let itemSchema = new mongoose.Schema({
   description: String,
   quantity: String,
-  units: String,
+  price: String,
   unitPrice: String,
-  totalPrice: String
-})
+  totalPrice: String,
+});
 
-let poSchema = new mongoose.Schema({
-  poId: { type: String, required: true },
-  keyPartnerId: { type: ObjectId, ref: 'user' },
-  poFrom: { type: String, required: true },
-  items: [itemSchema]
-}, { timestamps: true })
+let poSchema = new mongoose.Schema(
+  {
+    poId: { type: String, required: true },
+    keyPartnerId: { type: ObjectId, ref: "user" },
+    poFrom: { type: String, required: true },
+    items: [itemSchema],
+  },
+  { timestamps: true }
+);
 
-let PurchaseOrder = mongoose.model('purchase-order', poSchema)
+let PurchaseOrder = mongoose.model("purchase-order", poSchema);
 
-module.exports = PurchaseOrder
+module.exports = PurchaseOrder;
--- models/Quotation.js
+++ models/Quotation.js
@@ -1,22 +1,26 @@
-const mongoose = require('mongoose')
-const { ObjectId } = mongoose.Schema
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
 let itemSchema = new mongoose.Schema({
   description: String,
   unitPrice: String,
+  price: String,
   quantity: String,
-  totalPrice: String
-})
+  totalPrice: String,
+});
 
-let quotationSchema = new mongoose.Schema({
-  quotationId: { type: String, required: true },
-  keyPartnerId: { type: ObjectId, required: true, ref: 'user' },
-  quoteFrom: { type: String, required: true, ref: 'inquiry' },
-  items: [itemSchema],
-  status: { type: String, required: true },
-  validUntil: { type: String, required: true }
-}, { timestamps: true })
+let quotationSchema = new mongoose.Schema(
+  {
+    quotationId: { type: String, required: true },
+    keyPartnerId: { type: ObjectId, required: true, ref: "user" },
+    quoteFrom: { type: String, required: true, ref: "inquiry" },
+    items: [itemSchema],
+    status: { type: String, required: true },
+    validUntil: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-let Quotation = mongoose.model('quotation', quotationSchema)
+let Quotation = mongoose.model("quotation", quotationSchema);
 
-module.exports = Quotation
+module.exports = Quotation;
