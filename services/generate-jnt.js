ad2472e70f3f01aa56ce928c9783c16a69bf5580
EnzGyb <rdimasapit.bos@gmail.com>
2022-10-11 11:47:39 +0800

    change remarks value on jnt export

--- services/generate-jnt.js
+++ services/generate-jnt.js
@@ -10,6 +10,15 @@ module.exports = (booking, id) => {
     .then(async () => {
       let worksheet = workbook.getWorksheet(1)
       for(let j = 0; j < booking.length; j ++) {
+        let remarkVal;
+        if(booking[j].itemType === 'individual') {
+          remarkVal = `Color: ${booking[j].itemId.color.name}, Size: ${booking[j].itemId.size.name}`
+        } else if(booking[j].itemType === 'bundle') {
+          remarkVal = []
+          booking[j].bundleId.items.forEach(item => {
+            remarkVal.push(item.item)
+          })
+        }
         worksheet.getCell(`A${j + 9}`).value = booking[j].customer
         worksheet.getCell(`B${j + 9}`).value = booking[j].customerContact
         worksheet.getCell(`C${j + 9}`).value = `${booking[j].hsStNum}, ${booking[j].brgy}`
@@ -22,7 +31,7 @@ module.exports = (booking, id) => {
         worksheet.getCell(`J${j + 9}`).value = '1'
         worksheet.getCell(`K${j + 9}`).value = ''
         worksheet.getCell(`L${j + 9}`).value = booking[j].cod
-        worksheet.getCell(`M${j + 9}`).value = booking[j].remarks
+        worksheet.getCell(`M${j + 9}`).value = (booking[j].itemType === 'individual') ? remarkVal : remarkVal.join(', ')
       }
       await workbook.xlsx.writeFile(`./temp/${filename}`)
       new File({ filePath: `/temp/${filename}`, from: null }).save()
