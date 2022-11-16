76163ad05744503b0d734708667b8763a0044c9e
EnzGyb <rdimasapit.bos@gmail.com>
2022-11-16 21:19:31 +0800

    fix export booking file generation

--- services/generate-flash.js
+++ services/generate-flash.js
@@ -2,6 +2,7 @@ const Excel = require('exceljs')
 const File = require('../models/File')
 const moment = require('moment')
 const fs = require('fs')
+const Inventory = require('../models/Inventory')
 
 module.exports = (booking, id) => {
   let workbook = new Excel.Workbook()
@@ -12,12 +13,31 @@ module.exports = (booking, id) => {
       for(let j = 0; j < booking.length; j ++) {
         let itemLs = [];
         if(booking[j].itemType === 'individual') {
-          itemLs.push(booking[j].itemId.desc)
+          itemLs.push(`Color: ${booking[j].itemId.color.name}, Size: ${booking[j].itemId.size.name}`)
         } else if(booking[j].itemType === 'bundle') {
-          booking[j].bundleId.items.forEach(item => {
-            itemLs.push(item.item)
-          })
+          for(let k = 0; k < booking[j].bundleId.items.length; k++) {
+            let i = await Inventory.findById(booking[j].bundleId.items[k].itemId).populate({ path: 'color size' }).exec()
+            itemLs.push(`Name: ${i.desc}, Color: ${i.color.name}, Size: ${i.size.name}`)
+          }
         }
+
+        worksheet.getRow(j + 2).height = 15 * (itemLs.length * 2)
+
+        // worksheet.getCell(`A${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`B${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`C${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`D${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`E${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`F${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`G${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`H${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`I${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`J${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`K${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`L${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`M${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+        // worksheet.getCell(`O${j + 2}`).alignment = { vertical: 'top', horizontal: 'left' }
+
         worksheet.getCell(`A${j + 2}`).value = booking[j].bookingId
         worksheet.getCell(`B${j + 2}`).value = booking[j].customer
         worksheet.getCell(`C${j + 2}`).value = `${booking[j].hsStNum}, ${booking[j].brgy}, ${booking[j].city}, ${booking[j].province}`
@@ -31,10 +51,16 @@ module.exports = (booking, id) => {
         worksheet.getCell(`K${j + 2}`).value = ''
         worksheet.getCell(`L${j + 2}`).value = ''
         worksheet.getCell(`M${j + 2}`).value = (booking[j].itemType === 'individual') ? booking[j].itemId.desc : booking[j].bundleId.name
-        worksheet.getCell(`N${j + 2}`).value = (booking[j].itemType === 'individual') ? { richText: [{ text: `Color: ${booking[j].itemId.color.name}, ` }, { text: `Size: ${booking[j].itemId.size.name}` }] } : itemLs.join(', ')
+        
+        worksheet.getCell(`N${j + 2}`).value = (booking[j].itemType === 'individual') ? { richText: [{ text: `Color: ${booking[j].itemId.color.name}, ` }, { text: `Size: ${booking[j].itemId.size.name}` }] } : { richText: [{ text: itemLs.join(' \r\n') }] }
         worksheet.getCell(`O${j + 2}`).value = booking[j].cod
+
+        
+        worksheet.getCell(`N${j + 2}`).alignment = { wrapText: true }
+
+        
       }
-      await workbook.xlsx.writeFile(`./temp/${filename}`)
+      await workbook.xlsx.writeFile(`./temp/${filename}`, { useSharedStrings: true, useStyles: true })
       new File({ filePath: `/temp/${filename}`, from: null }).save()
       return { link: `/${filename}`, filename: filename }
     })
--- services/generate-jnt.js
+++ services/generate-jnt.js
@@ -2,6 +2,7 @@ const Excel = require('exceljs')
 const File = require('../models/File')
 const moment = require('moment')
 const fs = require('fs')
+const Inventory = require('../models/Inventory')
 
 module.exports = (booking, id) => {
   let workbook = new Excel.Workbook()
@@ -10,15 +11,18 @@ module.exports = (booking, id) => {
     .then(async () => {
       let worksheet = workbook.getWorksheet(1)
       for(let j = 0; j < booking.length; j ++) {
-        let remarkVal;
+        let remarkVal = []
         if(booking[j].itemType === 'individual') {
-          remarkVal = `Color: ${booking[j].itemId.color.name}, Size: ${booking[j].itemId.size.name}`
+          remarkVal.push(`Color: ${booking[j].itemId.color.name}, Size: ${booking[j].itemId.size.name}`)
         } else if(booking[j].itemType === 'bundle') {
-          remarkVal = []
-          booking[j].bundleId.items.forEach(item => {
-            remarkVal.push(item.item)
-          })
+          for(let k = 0; k < booking[j].bundleId.items.length; k++) {
+            let i = await Inventory.findById(booking[j].bundleId.items[k].itemId).populate({ path: 'color size' }).exec()
+            remarkVal.push(`Name: ${i.desc}, Color: ${i.color.name}, Size: ${i.size.name}`)
+          }
         }
+
+        worksheet.getRow(j + 9).height = 15 * (remarkVal.length * 2)
+
         worksheet.getCell(`A${j + 9}`).value = booking[j].customer
         worksheet.getCell(`B${j + 9}`).value = booking[j].customerContact
         worksheet.getCell(`C${j + 9}`).value = `${booking[j].hsStNum}, ${booking[j].brgy}`
@@ -31,9 +35,12 @@ module.exports = (booking, id) => {
         worksheet.getCell(`J${j + 9}`).value = '1'
         worksheet.getCell(`K${j + 9}`).value = ''
         worksheet.getCell(`L${j + 9}`).value = booking[j].cod
-        worksheet.getCell(`M${j + 9}`).value = (booking[j].itemType === 'individual') ? remarkVal : remarkVal.join(', ')
+        worksheet.getCell(`M${j + 9}`).value = (booking[j].itemType === 'individual') ? remarkVal[0] : remarkVal.join(' \r\n')
+      
+        worksheet.getCell(`M${j + 9}`).alignment = { wrapText: true }
+      
       }
-      await workbook.xlsx.writeFile(`./temp/${filename}`)
+      await workbook.xlsx.writeFile(`./temp/${filename}`, { useSharedStrings: true, useStyles: true })
       new File({ filePath: `/temp/${filename}`, from: null }).save()
       return { link: `/${filename}`, filename: filename }
     })
